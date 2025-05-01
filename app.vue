e0153241ba82500eb8f47a12915a7ee19d4a9688
saeed heidari <s.heidari@jabama.ir>
2025-05-01 14:55:05 +0330

    feat: add Tailwind CSS to project

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline text-center p-4">
+    Hello Jabama HR Admin!
+  </h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   devServer: {
     port: 3100
   }
