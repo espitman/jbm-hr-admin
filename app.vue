5e518b35bd0246a897496bbaace4566d77cab420
saeed heidari <s.heidari@jabama.ir>
2025-05-01 17:13:37 +0330

    feat: add CSS reference to style tags

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1 class="text-3xl font-bold underline text-center p-4">
+  <h1 class="text-3xl font-bold underline text-center p-4 hover:bg-red-500!">
     Hello Jabama HR Admin!
   </h1>
 </template>
+
+<style>
+@reference "~/assets/css/main.css";
+h1 {
+  @apply bg-blue-500;
+}
+</style>
