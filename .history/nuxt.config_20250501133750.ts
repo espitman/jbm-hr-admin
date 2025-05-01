7e176407fca0fe34ff8ba83e74446470019d1634
saeed heidari <s.heidari@jabama.ir>
2025-05-01 13:44:04 +0330

    chore: ignore .history directory

--- .history/nuxt.config_20250501133750.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui'],
-  server: {
-    port: 3100
-  }
-})
