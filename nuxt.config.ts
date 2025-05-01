00a866a527f9eefa7ba589b017a82763bc717cbb
saeed heidari <s.heidari@jabama.ir>
2025-05-01 14:53:27 +0330

    feat: initialize Nuxt 3 project with custom port configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  devServer: {
+    port: 3100
+  }
+})
